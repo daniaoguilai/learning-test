a71f357b63a131b39996e8604cba3e5b4ce7ca0d
weixudong <18811587621@163.com>
2019-08-15 11:36:34 +0800

    first game_functions war_star update

--- war_star/aline_invasion.py
+++ war_star/aline_invasion.py
@@ -3,7 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 
 def run_game():
     pygame.init()
@@ -14,9 +14,7 @@ def run_game():
     bg_color = ai_settings.bg_color
     ship = Ship(screen)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
         
         screen.fill(bg_color)
         ship.blitme()
