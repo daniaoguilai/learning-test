b76dc37107111fc19bf77d219b56e8315689517a
weixudong <18811587621@163.com>
2019-08-16 16:03:07 +0800

    add bullet$shotting

--- war_star/aline_invasion.py
+++ war_star/aline_invasion.py
@@ -11,7 +11,8 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
 
-    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+                                      ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     bg_color = ai_settings.bg_color
     ship = Ship(ai_settings,screen)
@@ -19,11 +20,12 @@ def run_game():
 
     bullets = Group()
     aliens = Group()
-    gf.creat_fleet(ai_settings, screen, aliens)
+    gf.creat_fleet(ai_settings, screen, ship,aliens)
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings,aliens)
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
--- war_star/game_functions.py
+++ war_star/game_functions.py
@@ -62,15 +62,37 @@ def get_number_aliens_x(ai_settings, alien_width):
     number_aliens_x = int(available_space_x / (2* alien_width))
     return number_aliens_x
 
-def creat_aline(ai_settings,screen,aliens,alien_number):
+def creat_aline(ai_settings,screen,aliens,alien_number,row_number):
     alien = Alien(ai_settings,screen)
     alien_width = alien.rect.width
     alien.x = alien_width +2* alien_width * alien_number
     alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2* alien.rect.height *row_number
     aliens.add(alien)
 
-def creat_fleet(ai_settings,screen,aliens):
+def creat_fleet(ai_settings,screen,ship,aliens):
     alien = Alien(ai_settings,screen)
     number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
-    for alien_number in range(number_aliens_x):
-        creat_aline(ai_settings,screen,aliens,alien_number)
+    number_rows = get_number_rows(ai_settings,ship.rect.height,
+                                  alien.rect.height)
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            creat_aline(ai_settings,screen,aliens,alien_number,row_number)
+
+def get_number_rows(ai_settings,ship_height,alien_height):
+    available_space_y = (ai_settings.screen_height -
+                         (3* alien_height) - ship_height)
+    number_rows = int(available_space_y / (2* alien_height))
+    return number_rows
+def check_fleet_edges(ai_settings,aliens):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings,aliens)
+            break
+def change_fleet_direction(ai_settings,aliens):
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+def update_aliens(ai_settings,aliens):
+    check_fleet_edges(ai_settings,aliens)
+    aliens.update()
