963cb9f5c9bcc3b0a3f21f9d2b71c1c44fea0e89
weixudong <18811587621@163.com>
2019-08-16 09:54:43 +0800

    add bullet$shotting

--- war_star/game_functions.py
+++ war_star/game_functions.py
@@ -1,30 +1,57 @@
 import sys
 import  pygame
+from  bullet import Bullet
 
-def update_screen(ai_settings,screen,ship):
-    """更新屏幕图像，并切换到新屏幕"""
-    #每次循环都要重绘屏幕
-    screen.fill(ai_settings.bg_color)
-    ship.blitme()
-    #让最近绘制的屏幕可见
-    pygame.display.flip()
+def fire_bullet(ai_settings,screen,ship,bullets):
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
-def check_events(ship):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ship)
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event, ship)
-
-def check_keydown_events(event,ship):
+def check_keydown_events(event,ai_settings,screen,ship,bullets):
         if event.key == pygame.K_RIGHT:
             ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            ship.moving_down = True
+        elif event.key == pygame.K_SPACE:
+            fire_bullet(ai_settings,screen,ship,bullets)
+        elif event.key == pygame.K_q:
+            sys.exit()
+
 def check_keyup_events(event,ship):
         if event.key == pygame.K_RIGHT:
             ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            ship.moving_down = False
+
+def check_events(ai_settings,screen,ship,bullets):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event,ai_settings,screen,ship,bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+def update_screen(ai_settings,screen,ship,bullets):
+    """更新屏幕图像，并切换到新屏幕"""
+    #每次循环都要重绘屏幕
+    screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    ship.blitme()
+    #让最近绘制的屏幕可见
+    pygame.display.flip()
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
--- war_star/ship.py
+++ war_star/ship.py
@@ -12,18 +12,26 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        self.center = float(self.rect.centerx)
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
         #移动标志
         self.moving_right = False
         self.moving_left = False
+        self.moving_down = False
+        self.moving_up = False
 
     def update(self):
         if self.moving_right and self.rect.right <= self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
+            self.centerx += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left >= 0:
-            self.center -= self.ai_settings.ship_speed_factor
+            self.centerx -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom <= self.screen_rect.bottom:
+            self.centery += self.ai_settings.ship_speed_factor
+        if self.moving_up and self.rect.top >= 0:
+            self.centery -= self.ai_settings.ship_speed_factor
 
-        self.rect.centerx = self.center
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
 
     def blitme(self):
         self.screen.blit(self.image,self.rect)
